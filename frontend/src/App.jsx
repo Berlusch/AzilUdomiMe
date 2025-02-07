146315efa088e6af7f11f88999bfbc584f0513e8
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-02-07 15:11:54 +0100

    Ponovno postavljanje Frontenda

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,15 +9,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+      <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+   
       </div>
-      <h1>Udomi me</h1>
+      <h2>Azil za pse</h2>
       <p className='grad'>Osijek</p>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           Vrijednost brojača je {count}
