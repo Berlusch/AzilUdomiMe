f6a3f3a805f491cd4deac597172aa21afbcfb8b1
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-02-14 17:56:30 +0100

    Dodavanje udomitelja

--- Frontend/src/Pages/Udomitelji/UdomiteljiDodaj.jsx
+++ Frontend/src/Pages/Udomitelji/UdomiteljiDodaj.jsx
@@ -1,21 +1,93 @@
-import { Col, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 import { RouteNames } from "../../constants";
+import UdomiteljService from "../../services/UdomiteljService";
 
 export default function UdomiteljiDodaj(){
+
+    const navigate = useNavigate();
+
+    async function dodaj(udomitelj){
+        const odgovor= UdomiteljService.dodaj(udomitelj);
+        if(odgovor.greska){
+            alert(odgovor.poruka)
+            return
+        }
+        navigate(RouteNames.UDOMITELJ_PREGLED)
+
+    }
+
+    function OdradiSubmit(e){ // e je event
+        e.preventDefault(); //nemoj odraditi zahtjev na server na standardni način
+        
+        let podatci = new FormData(e.target);
+
+        dodaj(
+            {            
+ 
+                ime: podatci.get('ime'),
+                prezime: podatci.get('prezime'),
+                adresa: podatci.get('adresa'),
+                telefon: podatci.get('telefon'),
+                email: podatci.get('email')
+            }
+             
+        
+        );
+    }
 
     
     return(
     <>
     Dodavanje udomitelja
+    <Form onSubmit={OdradiSubmit}>
+
+        <Form.Group controlId="ime">
+            <Form.Label>Ime</Form.Label>
+            <Form.Control type="text" name="ime" required/>
+        </Form.Group>
+
+        <Form.Group controlId="prezime">
+            <Form.Label>Prezime</Form.Label>
+            <Form.Control type="text" name="prezime" required/>
+        </Form.Group>
+
+        <Form.Group controlId="adresa">
+            <Form.Label>Adresa</Form.Label>
+            <Form.Control type="text" name="adresa" required/>
+        </Form.Group>
+
+        <Form.Group controlId="telefon">
+            <Form.Label>Telefon</Form.Label>
+            <Form.Control type="number" name="telefon" required/>
+        </Form.Group>
+
+        <Form.Group controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="text" name="email" required/>
+        </Form.Group>
+
+        <hr/>
+    
+
     <Row>
-        <Col>
+        <Col xs={6} sm={12} md={3} lg={6} xl={6} xxl={6}>
             <Link
             to={RouteNames.UDOMITELJ_PREGLED}
             className="btn btn-danger siroko"
             >Odustani</Link>
         </Col>
+
+        <Col xs={6} sm={12} md={9} lg={6} xl={6} xxl={6}>
+            <Button variant="success" type="submit" className="siroko">
+                Dodaj udomitelja
+            </Button>
+        </Col>
+
     </Row>
+
+
+    </Form>
     </>
     )
 }
--- Frontend/src/services/UdomiteljService.js
+++ Frontend/src/services/UdomiteljService.js
@@ -10,6 +10,13 @@ async function get(){
     .catch((e)=>{})
 }
 
+async function dodaj(udomitelj){
+    return HttpService.post('/Udomitelj', udomitelj)
+    .then(()=>{return{greska:false, poruka: 'Dodano'}})
+    .catch(()=>{return{greska:true, poruka:'Problem kod dodavanja'}})
+}
+
 export default{
-    get
+    get,
+    dodaj
 }
