05442a5d9746fb3013e401bd7d79933c5e934733
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-02-17 09:41:47 +0100

    Poruka za problem kod brisanja

--- Frontend/src/services/UdomiteljService.js
+++ Frontend/src/services/UdomiteljService.js
@@ -34,7 +34,7 @@ async function promijeni(sifra,udomitelj){
 async function obrisi(sifra,udomitelj){
     return HttpService.delete('/Udomitelj/'+sifra, udomitelj)
     .then(()=>{return{greska:false, poruka: 'Obrisano'}})
-    .catch(()=>{return{greska:true, poruka:'Problem kod brisanja'}})
+    .catch(()=>{return{greska:true, poruka:'Udomitelj ima aktualan upit te ga stoga nije moguće obrisati.'}})
 }
 
 export default{
