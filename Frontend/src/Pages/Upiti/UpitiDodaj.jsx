8472c09472b3ec8e8e940becf9b1f6033e60708d
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-02-22 09:10:26 +0100

    className umjesto class

--- Frontend/src/Pages/Upiti/UpitiDodaj.jsx
+++ Frontend/src/Pages/Upiti/UpitiDodaj.jsx
@@ -79,14 +79,14 @@ export default function UpitiDodaj(){
                 to={RouteNames.UPIT_PREGLED}
                 className="btn btn-danger siroko"
                 style={{ backgroundColor: '#9c989a' }}
-                >Odustani  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="24" fill="red" class="bi bi-x-lg" viewBox="0 0 16 16" stroke="red"><g transform="translate(2, 0)">
+                >Odustani  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="24" fill="red" className="bi bi-x-lg" viewBox="0 0 16 16" stroke="red"><g transform="translate(2, 0)">
                 <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/></g>
               </svg></Link>
             </Col>
 
             <Col xs={6} sm={12} md={9} lg={6} xl={6} xxl={6}>
                 <Button variant="success" type="submit" className="siroko" style={{ backgroundColor: '#7d3d9b' }}>
-                    Dodaj upit  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="26" fill="#00FF00" class="bi bi-check-lg" viewBox="0 0 16 16" stroke="#00FF00">
+                    Dodaj upit  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="26" fill="#00FF00" className="bi bi-check-lg" viewBox="0 0 16 16" stroke="#00FF00">
   <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
 </svg>
                 </Button>
