82e7b4f9bd8e60e0dd6db54a206f984d20dd376a
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-03-01 15:02:13 +0100

    Create Login.jsx

---
+++ Frontend/src/Pages/Login.jsx
@@ -0,0 +1,48 @@
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import useAuth from '../hooks/useAuth';
+
+export default function Login() {
+  const { login } = useAuth();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const podatci = new FormData(e.target);
+    login({
+      email: podatci.get('email'),
+      password: podatci.get('lozinka'),
+    });
+  }
+
+  return (
+    <Container className='mt-4'>
+        <p>
+            email: bernarda.lusch@gmail.com
+        </p>
+        <p>
+            lozinka: kadulja
+        </p>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className='mb-3' controlId='email'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type='text'
+            name='email'
+            placeholder='bernarda.lusch@gmail.com'
+            maxLength={255}
+            required
+          />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='lozinka'>
+          <Form.Label>Lozinka</Form.Label>
+          <Form.Control type='password' name='lozinka' required />
+        </Form.Group>
+        <Button variant='primary' className='gumb' type='submit'>
+          Autoriziraj
+        </Button>
+      </Form>
+    </Container>
+  );
+}
