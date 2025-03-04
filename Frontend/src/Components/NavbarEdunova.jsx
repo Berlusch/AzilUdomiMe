548e87995a10745ae30c8bf0a66282172c055507
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-03-04 15:50:52 +0100

    Update NavbarEdunova.jsx

--- Frontend/src/Components/NavbarEdunova.jsx
+++ Frontend/src/Components/NavbarEdunova.jsx
@@ -10,7 +10,7 @@ export default function NavBarEdunova() {
   const { logout, isLoggedIn } = useAuth();
 
   function OpenSwaggerURL() {
-    window.open(PRODUKCIJA + "/../../swagger/index.html", "_blank");
+    window.open(`${PRODUKCIJA}/../../swagger/index.html`, "_blank");
   }
 
   return (
@@ -21,69 +21,86 @@ export default function NavBarEdunova() {
           <Nav.Link onClick={() => navigate(RouteNames.HOME)}>
             <img src="/logo-removebg-preview.png" alt="Logo" className="logo-navbar" />
           </Nav.Link>
-        </Nav>
+        </Nav>        
 
-        {/* Ovdje stavimo flex na desnu stranu */}
-        <Nav
-          className="ml-auto" // Dodaj ml-auto za lijevo poravnanje svih linkova na desnu stranu
-          style={{
-            display: 'flex',           // Omogućava Flexbox
-            justifyContent: 'flex-end', // Poravnava sve na desnu stranu
-            width: '100%',              // Osigurava da zauzme cijeli prostor
-          }}
-        >
-          {isLoggedIn ? (
-            <>
-              <Nav.Link onClick={() => navigate(RouteNames.NADZORNA_PLOCA)}>
-                Nadzorna ploča
-              </Nav.Link>
+        {isLoggedIn && (
+          <Nav className="me-auto">
+            <Nav.Link onClick={() => navigate(RouteNames.NADZORNA_PLOCA)}>
+              <h4>Nadzorna ploča</h4>
+            </Nav.Link>
 
-              <NavDropdown
-                title={<span className="dropdown-title"><img src="/izbornik.png" alt="Izbornik" /></span>}
-                id="basic-nav-dropdown"
-              >
-                <NavDropdown.Item onClick={() => navigate(RouteNames.PAS_PREGLED)}>
-                  Psi
-                </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => navigate(RouteNames.UDOMITELJ_PREGLED)}>
-                  Udomitelji
-                </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => navigate(RouteNames.UPIT_PREGLED)}>
-                  Upiti
-                </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => navigate(RouteNames.STATUS_PREGLED)}>
-                  Statusi
-                </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => navigate(RouteNames.ERA_DIJAGRAM)}>
-                  ERA
-                </NavDropdown.Item>
-              </NavDropdown>
+            <NavDropdown
+              title={<span className="dropdown-title"><img src="/izbornik.png" alt="Izbornik" /></span>}
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item onClick={() => navigate(RouteNames.PAS_PREGLED)}>
+                Psi
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={() => navigate(RouteNames.UDOMITELJ_PREGLED)}>
+                Udomitelji
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={() => navigate(RouteNames.UPIT_PREGLED)}>
+                Upiti
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={() => navigate(RouteNames.STATUS_PREGLED)}>
+                Statusi
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={() => navigate(RouteNames.ERA_DIJAGRAM)}>
+                ERA
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        )}
+      </Navbar.Collapse>
 
-              <Nav.Link onClick={OpenSwaggerURL} style={{ display: 'block' }}>
-                <strong>Swagger</strong>
-              </Nav.Link>
-
-              <Nav.Link onClick={logout}>Odjava</Nav.Link>
-            </>
-          ) : (
+      <Nav
+        className="ml-auto"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-end',
+          gap: '10px',
+        }}
+      >
+        {isLoggedIn ? (
+          <>
             <Nav.Link
-              onClick={() => navigate(RouteNames.LOGIN)}
+              onClick={logout}
               style={{
                 display: 'inline-block',
-                color: '#4B0082',               // Boja teksta
-                backgroundColor: '#f0e6ff',    // Boja pozadine
-                padding: '10px 20px',          // Padding za gumb
-                borderRadius: '5px',           // Za zaobljene rubove
-                textAlign: 'center', 
-                marginRight: '10px',          // Poravnanje teksta
+                color: 'white',
+                backgroundColor: '#7f7e80',
+                padding: '10px 20px',
+                borderRadius: '5px',
+                textAlign: 'center',
+                height: '45px',
+                lineHeight: 'normal',
               }}
             >
-              <strong>Prijava</strong>
+              <strong>Odjava</strong>
             </Nav.Link>
-          )}
-        </Nav>
-      </Navbar.Collapse>
+
+            <Nav.Link onClick={OpenSwaggerURL}>
+              <strong>Swagger</strong>
+            </Nav.Link>
+          </>
+        ) : (
+          <Nav.Link
+            onClick={() => navigate(RouteNames.LOGIN)}
+            style={{
+              display: 'block',
+              color: '#4B0082',
+              backgroundColor: '#f0e6ff',
+              padding: '10px 20px',
+              borderRadius: '5px',
+              textAlign: 'center',
+              marginRight: '10px',
+            }}
+          >
+            <strong>Prijava</strong>
+          </Nav.Link>
+        )}
+      </Nav>
     </Navbar>
   );
 }
-
