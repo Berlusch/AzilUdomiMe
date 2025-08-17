cf098cd43a999d205e5cad1dbd7bc4a416b32c56
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-08-17 09:42:18 +0200

    Update EraDijagram.jsx

--- Frontend/src/Pages/Era/EraDijagram.jsx
+++ Frontend/src/Pages/Era/EraDijagram.jsx
@@ -4,7 +4,7 @@ const Slika = () => {
   return (
     <div className="slika-container">
         <h4>Udomi me ERA dijagram</h4>
-      <img src="/ERAdijagram.jpg" alt="Udomi me - ERA dijagram" className="era"/>
+      <img src="/diagram.jpg" alt="Udomi me - ERA dijagram" className="era"/>
     </div>
   );
 };
