6b1f20d7c21eb92774d5ec8777a1a39141817c8b
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-08-18 10:29:33 +0200

    Create UpitObrazacDTO.cs

---
+++ Backend/Models/DTO/UpitObrazacDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace Backend.Models.DTO
+{
+    /// <summary>    
+    /// Ovaj DTO se koristi za čitanje i unos podataka iz obrasca upita u bazu
+    /// </summary>  
+    /// <param name="Ime">Ime udomitelja.</param>
+    /// <param name="Prezime">Prezime udomitelja.</param>
+    /// <param name="Email">Email udomitelja.</param>
+    /// <param name="SadrzajUpita">Sadržaj upita.</param>
+    /// <param name="PasSifra">Šifra psa.</param>
+
+    public record UpitObrazacDTO
+    (
+        int PasSifra,
+        string Ime,
+        string Prezime,
+        string Email,
+        string SadrzajUpita
+    );
+}
