564cc599ae3040f9b51bc396b9f88e2bff9a90cf
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-08-23 12:27:59 +0200

    Create Lokacija.cs

---
+++ Backend/Models/Lokacija.cs
@@ -0,0 +1,33 @@
+﻿using Backend.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    /// <summary>
+    /// Klasa koja predstavlja lokaciju.
+    /// </summary>
+    public class Lokacija: Entitet
+
+    {
+        /// <summary>
+        /// Naziv lokacije.
+        /// </summary>
+        public string? Naziv { get; set; }
+        /// <summary>
+        /// Adresa lokacije.
+        /// </summary>
+        public string Adresa { get; set; } = null!;
+        /// <summary>
+        /// Grad lokacije.
+        /// </summary>
+        public string Grad { get; set; } = null!;
+        /// <summary>
+        /// Poštanski broj lokacije.
+        /// </summary>
+        public int PostanskiBroj { get; set; }
+
+        }
+}
+
+
+
