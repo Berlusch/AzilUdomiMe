007b56ce12d34150621554060714ba6bf56fecae
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-08-23 12:28:14 +0200

    Create Lokacija DTO-s

---
+++ Backend/Models/DTO/LokacijaDTOInsertUpdate.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.Models.DTO
+{
+    /// <summary>
+    /// DTO za unos i ažuriranje podataka o lokaciji.
+    /// </summary>
+    /// <param name="Naziv">Naziv lokacije (nije obavezno).</param>
+    /// <param name="Adresa">Adresa lokacije (obavezno, uključuje ulicu i broj).</param>
+    /// <param name="Grad">Grad lokacije (obavezno).</param>
+    /// <param name="PostanskiBroj">Poštanski broj lokacije (obavezno, 5 znamenki, prva ne smije biti nula).</param>
+    public record LokacijaDTOInsertUpdate
+    (
+        string? Naziv,
+        string Adresa,
+        string Grad,
+        int PostanskiBroj
+
+    );
+
+}
---
+++ Backend/Models/DTO/LokacijaDTORead.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.Models.DTO
+{
+    /// <summary>
+    /// DTO za čitanje podataka o lokaciji.
+    /// </summary>
+    /// <param name="Sifra">Jedinstvena šifra lokacije.</param>
+    /// <param name="Naziv">Naziv lokacije.</param>
+    /// <param name="Adresa">Adresa lokacije.</param>
+    /// <param name="Grad">Grad lokacije.</param>  
+    /// <param name="PostanskiBroj">Poštanski broj lokacije.</param>
+    public record LokacijaDTORead
+    (     
+        int Sifra,
+        string? Naziv,
+        string Adresa,
+        string Grad,
+        int PostanskiBroj
+        );
+}
