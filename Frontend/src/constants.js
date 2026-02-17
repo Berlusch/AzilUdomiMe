c3092f4e5b192e0ec9252ed3d0e724c534b1823a
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-02-17 10:34:55 +0100

    Edit production URL

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -30,7 +30,7 @@ export const RouteNames={
 }
 
 
-//export const PRODUKCIJA ='https://blusch-001-site1.qtempurl.com/api/v1';
+export const PRODUKCIJA ='https://solusch-001-site1.atempurl.com/api/v1';
 
-export const PRODUKCIJA ='https://localhost:7015/api/v1';
+//export const PRODUKCIJA ='https://localhost:7015/api/v1';
 
